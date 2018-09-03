871929914b8f38b10cadceceeac9ce0c71cc7550
zoghal <saleh.souzanchi@gmail.com>
2018-09-03 20:53:42 +0430

    update

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,45 +1,57 @@
 $(document).ready(function () {
 
-    $(" .main-slider").owlCarousel({
-        // autoPlay: true,
-        // stopOnHover: true,
-        // slideSpeed: 200,
-        // paginationSpeed: 800,
-        // rewindSpeed: 1000,
-        // navigationText: ["prev", "next"],
-        // rewindNav: true,
-        // singleItem: true,
-        // transitionStyle: "backSlide",
-        // responsive: true,
-        // responsiveRefreshRate: 200,
-        // responsiveBaseWidth: window,
+    $(".main-slider").owlCarousel({
         rtl: true,
-
-        items:1,
+        items: 1,
         autoplay: 1000,
         smartSpeed: 200,
         nav: false,
-
         animateIn: 'bounceInLeft',
         animateOut: 'bounceOutRight',
-        //responsive: true,
         loop: true,
-
         responsiveBaseElement: window
     });
 
     $(" .useablity-slider").owlCarousel({
-
         rtl: true,
-
-        items:2,
+        items: 2,
         nav: true,
-        navText: ['بعدی','قبلی'],
+        navText: ['بعدی', 'قبلی'],
         autoplay: false,
-        
-        
-
-        
     });
 
+
+    function getDonations() {
+        $.getJSON("donates/binama.json", function (data) {
+            var items = [];
+            $.each(data, function (key, val) {
+                var img = $('<img>').prop('src', 'https://s.gravatar.com/avatar/' + val.EmailMD5 + '?noemail&s=50&d=wavatar');
+                var name = $("<div/>").addClass('name').html(val.FullName);
+                if (val.CustomDescription === '#') {
+                    var user = $('<div>').addClass('user-donated').append(img, name);
+                } else {
+                    var user = $('<a>').addClass('user-donated').prop('href', val.CustomDescription).append(img, name);
+                }
+                $('#donated').append(user);
+
+            });
+        })
+    };
+
+    getDonations();
+
+
+    var heart = $('<i class="userIsLove fa fa-heart" aria-hidden="true"></i>');
+
+    $("#donated").on('mouseenter','.user-donated', function () {
+        $(this).append(heart);
+    });
+    $("#donated").on('mouseleave','.user-donated', function () {
+        $(this).find(".userIsLove").remove();
+    });
+    
+    
+    // , function () {
+    //     $(this).find("span:last").remove();
+    // });
 });
