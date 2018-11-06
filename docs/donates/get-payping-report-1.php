99c41a6a2cf91c91fa29bd280bba41ffd29e0221
Saleh Souzanchi <saleh.souzanchi@gmail.com>
2018-11-06 23:24:35 +0330

    new donnation

--- docs/donates/get-payping-report-1.php
+++ docs/donates/get-payping-report-1.php
@@ -1,80 +1,80 @@
 <?php
  $config = require('config.php');
 
- 
-
 function getItemCountPaymented()
 {
     global $config;
     $curl = curl_init();
-
     curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.payping.ir/v1/permaLink/".$config['paypingCode']."/BuyersListCount",
+            CURLOPT_URL => "https://api.payping.ir/v1/permalink/".$config['paypingCode']."/BuyersListCount",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "accept: application/json",
-                "authorization: Bearer ".$config['token'],
-                "cache-control: no-cache",
-            ),
+            CURLOPT_HTTPHEADER => array("accept: application/json", "authorization: Bearer ".$config['token'], "cache-control: no-cache"),
             ));
-
     $response = curl_exec($curl);
     $err = curl_error($curl);
-    print_r([$err,$response]);
+    $http_code = curl_getinfo($curl);
+    $http_code= $http_code['http_code'];
+    //  print_r([$http_code,$err,$response]);
     curl_close($curl);
     if ($err) {
-        die("cURL Error #:" . $err);
+        die("cURL Error: " . $err);
+    }
+
+    if($http_code !== 200){
+        die("Error: get response ".$http_code);
     }
 
     if (empty($response)) {
         die("Error: Can not fetch count payments");
     }
- 
+
     $s = json_decode($response);
-      // print_r($response);
-    return $s->count;
+    $s = $s->count;
+    return $s;
 }
 
 
 function getItemListPaymented($offset = 0, $limit = 50)
 {
     global $config;
-    $url = "https://api.payping.ir/v1/permaLink/".$config['paypingCode']."/BuyersList?offset=".$offset."&limit=".$limit;
-    echo "Start Featching : {$offset}=>{$limit}".PHP_EOL;
+    $url = "https://api.payping.ir/v1/permalink/".$config['paypingCode']."/BuyersList?offset=".$offset."&limit=".$limit;
+    echo "Start Featching : {$offset}=>{$limit}: ".$url.PHP_EOL;
 
     $curl = curl_init();
     curl_setopt_array($curl, array(
             CURLOPT_URL => $url,
-            
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 30,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "accept: application/json",
-                "authorization: Bearer ".$config['token'],
-                "cache-control: no-cache",
-            ),
-            ));
+            CURLOPT_HTTPHEADER => array("accept: application/json", "authorization: Bearer ".$config['token'], "cache-control: no-cache"),
+        ));
 
-    $response = curl_exec($curl);
+$response = curl_exec($curl);
     $err = curl_error($curl);
-    // print_r([$err,$response]);
+    $http_code = curl_getinfo($curl);
+    $http_code= $http_code['http_code'];
+    // print_r([$http_code,$err,$response]);
     curl_close($curl);
     if ($err) {
-        echo "cURL Error #:" . $err;
-        //return;
+        die("cURL Error: " . $err);
     }
+
+    if($http_code !== 200){
+        die("Error: get response ".$http_code);
+    }
+
     if (empty($response)) {
         die("Error: Can not fetch count payments");
     }
+   
 
     $s= json_decode($response);
     return $s;
@@ -82,14 +82,15 @@ function getItemListPaymented($offset = 0, $limit = 50)
 
 
 
-    
-     $total = getItemCountPaymented();
-      print_r($total);
 
-    $res = [];
-    for($i = 0; $i <= $total; $i=$i+50){
-        $list =  getItemListPaymented($i);
-        $res= array_merge( $res,$list);
-    }
+$total = getItemCountPaymented();
+print_r($total.PHP_EOL);
 
-   file_put_contents($config['AllDonates'], json_encode($res,JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE ) );
+$res = [];
+for($i = 0; $i <= $total; $i=$i+50){
+$list =  getItemListPaymented($i);
+$res= array_merge( $res,$list);
+}
+print_r(count($res));
+
+file_put_contents($config['AllDonates'], json_encode($res,JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE ) );
